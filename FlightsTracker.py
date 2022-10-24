5d9be6646348f4e033708b408897d40d11228cb2
Francis Massolin <114305865+datafoj@users.noreply.github.com>
2022-10-24 22:28:33 +0400

    Mise a jour de l'application
    Version Beta - almost there !!!

--- FlightsTracker.py
+++ FlightsTracker.py
@@ -1,5 +1,6 @@
-#IMPORT LIBRARY
-from pprint import pprint
+#!/Users/Francis/Desktop/Python3.9/Training/venv/bin/python3
+# -*- encoding: utf8 -*-
+# Importation of Python libraries
 import pandas as pd
 from IPython.display import display
 import requests
@@ -7,15 +8,24 @@ import urllib.request
 import json
 import ssl
 from bokeh.plotting import figure, show, ColumnDataSource
-from bokeh.tile_providers import STAMEN_TONER
+from bokeh.layouts import column, row
+from bokeh.client import push_session
+from bokeh.models import HoverTool, WMTSTileSource, LinearColorMapper, LabelSet, Label
+from bokeh.tile_providers import STAMEN_TERRAIN_RETINA, get_provider
+from bokeh.application import Application
+from bokeh.application.handlers.function import FunctionHandler
+from bokeh.server.server import Server
+from bokeh.io import curdoc
 import numpy as np
+import folium
+
 
 # Header
 ssl._create_default_https_context = ssl._create_unverified_context
 opener = urllib.request.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
-# Provide your API key in the query string:
+# Provide your API key in the query string!!!
 params = {
   'api_key': '9d2955c1-020a-4343-a6d7-c2f04450735b'
 }
@@ -36,33 +46,64 @@ api_flights = api_response["response"]
 with open("flights.json", "w") as fp:
     json.dump(api_flights, fp, indent=4, sort_keys=True)
 
-#Load as JSON into a dataframe:
+# Load as JSON into a dataframe:
 df = pd.read_json("flights.json")
 # print with pandas
 #display(df)
 
-#Convert to Pandas Dataframe
-flight_df = pd.DataFrame(df, columns = ['lat', 'lng', 'alt', 'dep_iata', 'arr_iata', 'airline_iata'])
-display(flight_df)
-"""# Define country coordinate range with Mercator method (EPSG:3857)
-x_range,y_range=([-15187814,-6458032], [2505715,6567666])
+# Convert to Pandas Dataframe
+flights_df = pd.DataFrame(df, columns = ['lat', 'lng', 'alt', 'flight_icao', 'dep_iata', 'arr_iata', 'airline_iata'])
+display(flights_df)
 
-#Function to convert coordinate to web Mercator
-def wgs84_to_web_mercator(df, lon="Long", lat="Lat"):
-    k = 6378137
-    df["x"] = df[lon] * (k * np.pi/180.0)
-    df["y"] = np.log(np.tan((90 + df[lat]) * np.pi/360.0)) * k
+# Convert France GPS coordinates to Web Mercator
+x_range, y_range = ((-6.403614, 12.229198), (40.587984, 53.007425)) # FRANCE GPS coordinates location
+
+# Convert All GPS coordinates to Web Mercator
+def mercator(df, lat="lat", lon="lng"):
+    radius = 6378137 # radius of the earth in meters
+    df["x"] = df[lon] * (radius * np.pi/180.0)
+    df["y"] = np.log(np.tan((90 + df[lat]) * np.pi/360.0)) * radius
     return df
-wgs84_to_web_mercator(flight_df)
 
-#Bokeh column data source
-flight_source=ColumnDataSource(flight_df)
+# Flights tracking main function
+def flights_tracker(geo):
+    # Bokeh column data source
+    mercator_df = mercator(flights_df)
+    flights_source = ColumnDataSource(mercator_df)
+    display(flights_source)
 
-#Setup figure
-p=figure(x_range=x_range,y_range=y_range,x_axis_type='mercator',y_axis_type='mercator',
-  sizing_mode='scale_width',plot_height=300)
-p.add_tile(STAMEN_TONER)
-p.circle('x','y',source=flight_source,fill_color="blue",size=10,fill_alpha=0.8,line_width=0.5)
+    def streaming():
+        # Map coordinates:
+        # Objet MAP: https://docs.bokeh.org/en/latest/docs/reference/tile_providers.html
+        map = figure(tools = 'pan, zoom_in, zoom_out, wheel_zoom, reset, save',
+        x_range = x_range, y_range = y_range, x_axis_type = 'mercator',
+          y_axis_type = 'mercator', sizing_mode = 'scale_width', plot_height = 300)
 
-#Display figure
-show(p)"""
+        # Add planes geolocalisation and informative labels object:
+        infos = HoverTool()
+        infos.tooltips = [('Flight', '@flight_icao'), ('Altitude', '@alt'),
+         ('Departure', '@dep_iata'), ('Arrival', '@arr_iata'), ('Airline', '@airline_iata')]
+        labels = LabelSet(x = 'x', y = 'y', text = 'flight_icao', level = 'glyph', x_offset = 5,
+         y_offset = 5, source = flights_source, render_mode = 'canvas',
+         background_fill_color = 'skyblue', text_color = 'black', text_font_size = '8pt')
+
+        # Add the WMTS tile source to the map object:
+        map_provider = (get_provider(STAMEN_TERRAIN_RETINA))
+        map.add_tile(map_provider) # bokeh.tile_providers.StamenTerrainRetina
+        map.add_tools(infos)                # Fucking doesn't work!!!
+        map.circle('x', 'y', source = flights_source, fill_color = "blue", size = 10, fill_alpha = 0.8, line_width = 0.5)
+        map.add_layout(labels)
+
+        curdoc().title = "Flights Tracker"
+        curdoc().add_root(map())
+        session = push_session(curdoc())
+        session.show()
+
+#Display the Map in the browser:
+#show(map)
+
+# Server initialization for Bokeh Application (Flights Tracker) on port 48010:
+# To run through bash terminal: bokeh serve --show FlightsTracker.py
+apps = {'/': Application(FunctionHandler(flights_tracker))}
+server = Server(apps, port=48010) # Any unassigned port will do
+server.io_loop.start()
